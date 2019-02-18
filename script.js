0bb7fc4807b61dc03fbb5d07a8c0db4532eca75d
Adam Frank White <adamfrankwhite@gmail.com>
2019-02-18 13:28:40 +0000

    Added updateMap function

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 // Map Function
 var button = document.querySelector('button');
 
-button.onclick = (function () {
+button.onclick = function () {
 	getPostcode()
+}
 
 function getPostcode() {
 	postcode = document.getElementById('postcode').value;
@@ -14,21 +15,23 @@ function getPostcode() {
 	request.open('GET', pcode)
 	request.onload = function () {
 		var results = JSON.parse(request.responseText)
-		lat = results.result.latitude;
-		lng = results.result.longitude;
-		console.log(lat, lng)
+		latitude = results.result.latitude;
+		longitude = results.result.longitude;	
+		updateMap(latitude,longitude) //updates map position to postcode
 	}
 	
 	request.send();
+	
+	
 }		
-})
 
 
 
-function initMap() {
+
+function initMap(latitude=53.3933, longitude=-2.1266, zoomValue=6) { // add default coordinate values, or pass in updated coordinates
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 53.3933, lng: -2.1266},
-		zoom: 7,
+		center: {lat: latitude, lng: longitude},
+		zoom: zoomValue,
 		mapTypeControlOptions: {
 			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
 		},
@@ -58,17 +61,19 @@ function initMap() {
 		
 	  });
 		// create html for marker info
-		let contentString = `<div><h3 class="heading">${castle.name}</h3>` +
-	`<h4>${castle.county}</h4>` + 
+		let contentString = 
+	
+	`<div><h3 class="heading"><a href="${castle.website}" target="_blank">${castle.name}</a></h3>` +
+	`<h4><i>${castle.county}</i></h4>` + 
 	`<img src='images/thumbs/${castle.img}'>` +
-	`<h4 class="sub">Built: ${castle.built}</h4>` +
-	`<h4 class="sub">Status: ${castle.status}</h4>` +
-	`<h4 class="sub">Postcode: ${castle.postcode}</h4></div>`
+	`<h4 class="sub">Built: <p>${castle.built}</p></h4>` +
+	`<h4 class="sub">Status: <p>${castle.status}</p></h4>` +
+	`<h4 class="sub">Postcode: <p>${castle.postcode}</p></h4></div>`
 		// create info window - - use of let VITAL, see above
 		let infowindow = new google.maps.InfoWindow({
 			content: contentString
 	})
-		console.log(marker)
+		
 		marker.addListener('click', function () {
 			infowindow.close()
 			infowindow.open(map, marker)
@@ -80,6 +85,10 @@ function initMap() {
 	
 }
 
+function updateMap(longitude, latitude) {
+	initMap(longitude, latitude, 9) // updates zoom
+}
+
 
 // append to dom card for each shelter within x 5miles, chosen from drop dwon menubar
 // need to add all shelters to list - 
@@ -89,6 +98,9 @@ function initMap() {
 
 // have media query at medium size to place map on left, with info box on right?
 
+// have more>> button (next to county?), that appends div to the right of window, with short description
+
 // when entered postcode, center map and display closest five castle on the side/bottom?
   
 // this type of app has real marketable value - think seeds or plant search, where you can choose your type, and find all nearby stockists
+
