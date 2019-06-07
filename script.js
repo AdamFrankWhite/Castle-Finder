338cccd2046bf0b90b59bd0d11418d18e3e4d755
Adam Frank White <adamfrankwhite@gmail.com>
2019-06-07 21:31:33 +0100

    altered nearby castle functionality

--- script.js
+++ script.js
@@ -23,61 +23,61 @@ function showError() {
 }
 
 function createMarkerContent(castle) {
-	let contentString = 
+	let contentString =
 	    `<div class="marker-window"><h3 class="heading"><a href="${castle.website}" target="_blank">${castle.name}</a></h3>` +
 	// div class for media queries
-        `<h4 class="sub_county"><i>${castle.county}</i></h4>` + 
+        `<h4 class="sub_county"><i>${castle.county}</i></h4>` +
         `<img src='images/thumbs/${castle.img}'>` +
         `<h4 class="sub_built">Built: <p>${castle.built}</p></h4>` +
         `<h4 class="sub_status">Status: <p>${castle.status}</p></h4>` +
         `<h4 class="sub_post">Postcode: <p>${castle.postcode}</p></h4>` +
         `<h4 class="more-info"><a target="_blank" href="${castle.website}">Visit Website</a></h4>
-			</div>` 
+			</div>`
   return contentString;
 }
 
 function getPostcode() {
 	var postcode = document.getElementById('postcode').value;
 	var pcode = "https://api.postcodes.io/postcodes/" + postcode
-	
+
 	//Validate input
 	if (postcode === "") {
 		showError();
 		errorMessage.textContent = "Please enter a valid UK postcode";
 	} else {
 		// AJAX request
-		
+
 		let request = new XMLHttpRequest();
 		request.open('GET', pcode)
 		request.onload = function () {
 			var results = JSON.parse(request.responseText);
-			
+
 			//Success
 			if (request.status === 200) {
 				var latitude = results.result.latitude;
-				var longitude = results.result.longitude;	
+				var longitude = results.result.longitude;
 				updateMap(latitude,longitude) //updates map position to postcode
 				hideError();
 				showNearest(latitude, longitude);
-				
-			//Invalid postcode	
+
+			//Invalid postcode
 			} else if (request.status === 404){
 				showError();
 				errorMessage.textContent = `${postcode} is not a valid UK postcode. Please try again.`;
-				
+
 			//Other errors
 			} else {
 				showError();
 				errorMessage.textContent = 'Error. Please try again.';
 			}
 		} // onload end
-		
+
 		request.send();
 		}
-}		
+}
 
 function initMap(latitude=53.3933, longitude=-2.1266, zoomValue=6) { // default values for initial load
-	
+
 	// === Map Object ===
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: latitude, lng: longitude},
@@ -91,39 +91,43 @@ function initMap(latitude=53.3933, longitude=-2.1266, zoomValue=6) { // default 
 					{saturation: -50}
 				]}
 			]
-	})	
-	
+	})
+
 	// === Markers ===
-	
+
 	//Icon
 	var image = {
 		url: 'images/icon.png',
 		size: new google.maps.Size(70, 70)
 	}
-	
+
 	//Create marker and infoWindow for each castle
 	for (var i=0; i<castles.length; i+=1) {
 		//Marker
-		let castle = castles[i];    
-		let marker = new google.maps.Marker({  
+		let castle = castles[i];
+		let marker = new google.maps.Marker({
 			position: {lat: castle.pos[0], lng: castle.pos[1]},
 			map: map,
 			icon: image,
-			content: createMarkerContent(castle) 
+			content: createMarkerContent(castle)
 	  });
 		markers.push(marker);
-	
-		
-    //infoWindow - - use of var VITAL here, to ensure only one infowindow open at a time    
+
+
+    //infoWindow - - use of var VITAL here, to ensure only one infowindow open at a time
 		var infowindow = new google.maps.InfoWindow({
-			content: createMarkerContent(castle) 
+			content: createMarkerContent(castle),
+			pixelOffset: new google.maps.Size(-15,0)
 	})
-		
+
 		// Marker click event
     google.maps.event.addListener(marker, 'click', function(){
+			map.setCenter(marker.getPosition());
       infowindow.setContent(this.content);
-      infowindow.open(map,this);})
-						
+      infowindow.open(map,this);
+		})
+			// console.log(this)
+
 	}
 }
 
@@ -139,17 +143,22 @@ function showNearest(latitude, longitude){
 	let outputHTML = ""
 	for (let i=0; i<5; i++) {
 		let distance = (Math.sqrt(((latitude - castlesArray[i].pos[0])**2) + ((longitude - castlesArray[i].pos[1])**2))*69).toFixed(1) // approx 69 miles per degree of lat/long
-	
-		outputHTML += `<div class="castleItem">
-										<h3>${castlesArray[i]["name"]}</h3>
-										<h3>${castlesArray[i]["built"]}</h3>
-										<img class="thumb" src="images/thumbs/${castlesArray[i]["img"]}">
-										<h3>Distance: ${distance} miles</h3>
+		let lat = castlesArray[i].pos[0];
+		let lon = castlesArray[i].pos[1];
+		outputHTML += `<div class="castleItem" onclick="updateMap(${lat}, ${lon})">
+										<div>
+											<img class="thumb" src="images/thumbs/${castlesArray[i]["img"]}">
+										</div>
+										<div class="castleInfo">
+											<h5 style="font-weight: bold;">${castlesArray[i]["name"]}</h5>
+											<h5>${castlesArray[i]["built"]}</h5>
+											<h5>Distance: ${distance} miles</h5>
+										</div>
 									</div>
 									`
 	}
-	console.log(outputHTML)
-	nearestDiv.innerHTML = outputHTML
+
+	nearestDiv.innerHTML = "<h2 class='nearbyHead'>Nearby Castles</h2>" + outputHTML
 }
 
 // Type ahead
@@ -162,7 +171,7 @@ function findMatches(termToMatch, castles) {
 }
 
 function clearSearch(){
-	suggestions.innerHTML = ""; 
+	suggestions.innerHTML = "";
 }
 
 function updateSearchValue(castle) {
@@ -175,21 +184,25 @@ function openMarker(index) {
 		url: 'images/icon.png',
 		size: new google.maps.Size(70, 70)
 	}
-	let castle = castles[index];    
+	let castle = castles[index];
 	let marker = markers[index]
 	console.log(castle)
-  //infoWindow - - use of var VITAL here, to ensure only one infowindow open at a time    
+  //infoWindow - - use of var VITAL here, to ensure only one infowindow open at a time
 	let infowindow = new google.maps.InfoWindow({
-		content: createMarkerContent(castle) 
+		content: createMarkerContent(castle)
 	})
 		//Marker click event
 	infowindow.setContent(this.content);
   infowindow.open(map,marker);
 	console.log("boo")
-		 
-  
+
+
 }
 
+function myClick(index) {
+    google.maps.event.trigger(markers[index]);
+		console.log("bla")
+  }
 
 function displayMatches() {
 	const matchesArray = findMatches(this.value, castles);
@@ -199,11 +212,11 @@ function displayMatches() {
 		let index = castles.indexOf(castle)
 		return `
 		<li>
-	<span onclick="updateMap(${latitude}, ${longitude}); updateSearchValue('${castle.name}'); clearSearch(); openMarker(${index})">${castle.name}, ${castle.county}, ${castle.postcode}</span>
+	<span onclick="updateMap(${latitude}, ${longitude}); showNearest(${latitude}, ${longitude}); myClick(${index}); updateSearchValue('${castle.name}'); clearSearch(); openMarker(${index}); hideError();">${castle.name}, ${castle.county}, ${castle.postcode}</span>
 		</li>
 	`}).join(""); // .join vital to avoid comma bug - happens since using innerHTML to assign an array - this way the mapped output is changed to string before inserting as HTML
 	suggestions.innerHTML = html; // add click event to focus on castle
-	// to do - move click events to js function, including one passing castle object, to then create marker 
+	// to do - move click events to js function, including one passing castle object, to then create marker
 }
 
 const searchInput = document.querySelector('#postcode');
@@ -220,5 +233,5 @@ searchInput.addEventListener('keyup', displayMatches);
 // have more>> button (next to county?), that appends div to the right of window, with short description
 
 // when entered postcode, center map and display closest five castle on the side/bottom?
-  
+
 // this type of app has real marketable value - think seeds or plant search, where you can choose your type, and find all nearby stockists
